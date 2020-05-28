a5b0998e87bc8af75f71ad202e71222b0dca85e6
Qin <1044471292@qq.com>
2020-05-28 10:21:08 +0800

    fix: 正则表达式修复 0:mm:ss 时截取字符串出现问题

--- src/main/java/timer/talkrecordtransfer/TransHelper.java
+++ src/main/java/timer/talkrecordtransfer/TransHelper.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author qin
@@ -17,15 +19,20 @@ public class TransHelper {
 
     private final static Logger logger = LoggerFactory.getLogger(TransHelper.class);
 
-    public static List<Talk> trans(){
+    private final static int LINE_TO_SKIP = 2140;
+
+    public static List<Talk> trans() {
         List<Talk> talks = new LinkedList<>();
         File file = new File("C:\\Users\\10444\\Desktop\\＋＋日谈.txt");
         BufferedReader bufferedReader = null;
         try {
             bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            String tempTimeAndTeller="2020-05-22 22:00:00";
+            String tempTimeAndTeller = "2020-05-22 22:00:00";
 //            String tempStory = "";
             String tempStr;
+            for (int i = 0; i < LINE_TO_SKIP; i++) {
+                bufferedReader.readLine();
+            }
             while ((tempStr = bufferedReader.readLine()) != null) {
                 if (tempStr.length() < 4) {
                     continue;
@@ -34,22 +41,35 @@ public class TransHelper {
                     tempTimeAndTeller = tempStr;
                     continue;
                 }
-                if(tempStr.length()<50){
+                if (tempStr.length() < 50) {
                     continue;
                 }
                 Talk talk = new Talk();
-                String tempTime = tempTimeAndTeller.substring(0,19);
-                String tempTeller = tempTimeAndTeller.substring(20);
 
-                if(tempTeller.equals("(80000000)")){
-                    talk.setTeller("匿名");
+                String regEx = "((20[0-9]{2})-(0?[1-9]|1[0-2])-((0?[1-9])|([12][0-9])|30|31) ([0]|[12][0-9]):((0?[1-9])|([1-5][0-9])):((0?[1-9])|([1-5][0-9])))" +
+                        " (.*)\\(.*\\)";
+//    String regEx = "[0-9]{4}-[0-9]{2}-[0-9]{2}";
+                Pattern pattern = Pattern.compile(regEx);
+                Matcher matcher = pattern.matcher(tempTimeAndTeller);
+//                int splitPoint = matcher.end();
+//                String tempTime = tempTimeAndTeller.substring(0,splitPoint);
+//                String tempTeller = tempTimeAndTeller.substring(splitPoint+1);
+//                System.out.println(tempTimeAndTeller);
+                if (matcher.matches()) {
+                    String tempTime = matcher.group(1);
+                    String tempTeller = matcher.group(14);
+//                    System.out.println(tempTime);
+//                    System.out.println(tempTeller);
+                    if (tempTeller.equals("")) {
+                        talk.setTeller("匿名");
 //                    System.out.println("匿名");
-                }else {
-                    int endIdx = tempTeller.indexOf('(');
-                    talk.setTeller(tempTeller.substring(0,endIdx));
-//                    System.out.println(tempTeller.substring(0,endIdx));
+                    } else {
+                        talk.setTeller(tempTeller);
+                    }
+                    talk.setTime(tempTime);
                 }
-                talk.setTime(tempTime);
+
+
                 talk.setContent(tempStr);
 //                System.out.println(tempTime);
 //                System.out.println(tempStr);
